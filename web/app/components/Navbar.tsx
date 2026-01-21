a53447f00f43be6c20f077401cc728c95516ad70
Warren Roberts <wpersonal04@gmail.com>
2026-01-20 21:14:33 -0600

    Edited the navagation bar and the homepage, attempted replication of current homepage, with some differences.

--- web/app/components/Navbar.tsx
+++ web/app/components/Navbar.tsx
@@ -3,18 +3,18 @@ import Link from 'next/link'
 
 const Navbar = () => {
     return (
-        <nav className="fixed top-0 w-full flex items-center justify-around py-5 px-24 border-b border-gray-700 bg-black">
+        <nav className="fixed top-0 w-full flex items-center justify-around py-3 px-24 border-b border-gray-700 bg-transparent backdrop-blur-md z-50">
 
             <Link
 
                 href="/"
-                className="transition duration-300 hover:scale-110">
+                className="mr-auto transition hover:scale-110">
 
                 <img
                     src="/CWAlogo-lg.gif"
                     alt="Home"
-                    width={64}
-                    height={64}
+                    width={128}
+                    height={128}
                     className="object-contain"
                 />
 
@@ -22,12 +22,12 @@ const Navbar = () => {
 
             {/* Each of these links is a page that can be routed to from the navbar */}
 
-            <ul className="flex gap-10 text-lg font-medium text-gray-300">
+            <ul className="flex gap-10 text-xl font-medium text-black">
 
                 <Link
 
                     href="/stats_and_titles"
-                    className="text-gray-300 hover:text-white transition-colors">
+                    className="text-black-300 hover:text-white transition-colors">
 
                     Stats & Titles
 
@@ -36,7 +36,7 @@ const Navbar = () => {
                 <Link
 
                     href="/getting_started"
-                    className="text-gray-300 hover:text-white transition-colors">
+                    className="text-black-300 hover:text-white transition-colors">
 
                     Getting Started
 
@@ -45,16 +45,16 @@ const Navbar = () => {
                 <Link
 
                     href="/news"
-                    className="text-gray-300 hover:text-white transition-colors">
+                    className="text-black-300 hover:text-white transition-colors">
 
                     News
 
-                </Link>  
+                </Link>
 
                 <Link
 
                     href="/events"
-                    className="text-gray-300 hover:text-white transition-colors">
+                    className="text-black-300 hover:text-white transition-colors">
 
                     Events
 
@@ -63,11 +63,11 @@ const Navbar = () => {
                 <Link
 
                     href="/rules_and_grading"
-                    className="text-gray-300 hover:text-white transition-colors">
+                    className="text-black-300 hover:text-white transition-colors">
 
                     Rules & Grading
 
-                </Link>                              
+                </Link>
             </ul>
 
         </nav>
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -4,22 +4,78 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main
-      className="
+
+    <main>
+
+      {/* Overlay */}
+
+      <section
+        className="
         min-h-screen
         bg-[url('/homepage_background.jpg')]
         bg-cover
-        bg-center
+        bg-[center_20%]
         bg-no-repeat
         flex
-        items-center
-        justify-center
+        flex-col
       "
-    >
-      <h1 className="text-white text-5xl font-bold">
-        Continental Whippet Alliance
-      </h1>
+      >
 
+        {/* Title */}
+
+        <div className="flex flex-1 items-center justify-center">
+          <h1 className="text-white text-5xl font-bold">
+            <span className="block">
+              Showing what we race.
+            </span>
+            <span className="block pl-14">
+              Racing what we show.
+            </span>
+          </h1>
+        </div>
+
+        {/* bottom fade */}
+        <div className="pointer-events-none absolute inset-x-0 bottom-0 h-15 bg-gradient-to-b from-transparent to-neutral-400" />
+
+      </section>
+
+
+
+
+      {/*Post Image, scrolling to view about information and below footer*/}
+
+      <section className="bg-neutral-400 text-black py-50">
+        <div className="max-w-3xl mx-auto px-">
+          <h2 className="text-4xl font-semibold">About Us</h2>
+          <p className="mt-3 text-lg text-zinc-900">
+            The <b>Continental Whippet Alliance (CWA)</b> was established in 1990. The primary mission of the CWA is to promote, protect and preserve purebred Whippet
+            racing and to provide a friendly and enjoyable environment for sportsmanlike competition. It is the objective of the CWA to play a role in the preservation
+            of the Whippet's athletic ability, sporting instincts and functional breed characteristics; to foster future generations of fit, versatile individuals that
+            are true to the AKC Whippet Breed Standard.
+          </p>
+        </div>
+      </section>
+
+
+
+
+
+
+
+
+
+      {/* Footer Section */}
+
+      <footer className="bg-neutral-400 pb-6">
+        <p className="text-white text-sm text-center leading-relaxed">
+          <span className="block">
+            Questions? Email cwawhippetracing@gmail.com
+          </span>
+          <span className="block">
+            © 2026 Continental Whippet Alliance. All rights reserved.
+          </span>
+        </p>
+      </footer>
 
     </main>
   )
