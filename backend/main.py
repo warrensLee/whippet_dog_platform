30529ab932fada50e201dd288ac4be75c57d7a07
Brayden Campbell <brayden.campbell100@gmail.com>
2026-01-20 22:15:59 -0600

    flask instance/nginx conf

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,13 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
 
-print("hi")
-while True:
-    pass
+@app.get("/")
+def home():
+    return jsonify(status="ok", message="Flask is running")
+
+@app.get("/health")
+def health():
+    return "OK", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
