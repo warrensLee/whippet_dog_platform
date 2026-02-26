95f2615252a8171f5530767538be8ea00251a043
Kevin Van <kevinv@uark.edu>
2026-02-26 00:03:50 -0600

    Added to importer files

--- backend/controller/importer.py
+++ backend/controller/importer.py
@@ -34,6 +34,33 @@ def import_csv():
 
     elif import_type == "race_results":
         deny = require_scope(role.edit_race_results_scope, "import race results")
+    
+    elif import_type == "dog_owners":
+        deny = require_scope(role.edit_dog_owner_scope, "import dog owners")
+
+    #elif import_type == "dog_titles":
+    #    deny = require_scope(role.edit_dog_title_scope, "import dog titles")
+
+    elif import_type == "news":
+        deny = require_scope(role.edit_news_scope, "import news")
+
+    elif import_type == "officer_roles":
+        deny = require_scope(role.edit_officer_role_scope, "import officer roles")
+
+    elif import_type == "persons":
+        deny = require_scope(role.edit_person_scope, "import persons")
+
+    elif import_type == "clubs":
+        deny = require_scope(role.edit_club_scope, "import clubs")
+
+    #elif import_type == "change_logs":
+    #    deny = require_scope(role.edit_change_log_scope, "import change logs")
+
+    elif import_type == "title_types":
+        deny = require_scope(role.edit_title_type_scope, "import title types")
+
+    elif import_type == "user_roles":
+        deny = require_scope(role.edit_user_role_scope, "import user roles")
 
     else:
         return jsonify({"ok": False, "error": "Invalid import type"}), 400
@@ -45,6 +72,15 @@ def import_csv():
             or (import_type == "meets" and role.edit_meet_scope != UserRole.ALL)
             or (import_type == "meet_results" and role.edit_meet_results_scope != UserRole.ALL)
             or (import_type == "race_results" and role.edit_race_results_scope != UserRole.ALL)
+            or (import_type == "dog_owners" and role.edit_dog_owner_scope != UserRole.ALL)
+            #or (import_type == "dog_titles" and role.edit_dog_title_scope != UserRole.ALL)
+            or (import_type == "news" and role.edit_news_scope != UserRole.ALL)
+            or (import_type == "officer_roles" and role.edit_officer_role_scope != UserRole.ALL)
+            or (import_type == "persons" and role.edit_person_scope != UserRole.ALL)
+            or (import_type == "clubs" and role.edit_club_scope != UserRole.ALL)
+            #or (import_type == "change_logs" and role.edit_change_log_scope != UserRole.ALL)
+            or (import_type == "title_types" and role.edit_title_type_scope != UserRole.ALL)
+            or (import_type == "user_roles" and role.edit_user_role_scope != UserRole.ALL)
         ):
         return jsonify({
             "ok": False,
@@ -75,5 +111,6 @@ def import_csv():
 def get_import_types():
     return jsonify({
         "ok": True,
-        "import_types": ["dogs", "meets", "meet_results", "race_results"],
+        "import_types": ["dogs", "meets", "meet_results", "race_results", "dog_owners", "dog_titles",
+                         "news", "officer_roles", "persons", "clubs", "change_logs", "title_types", "user_roles"],
     }), 200
