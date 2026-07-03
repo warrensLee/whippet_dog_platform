d75c96854a2603ae86455e081aa517dde36675b5
Dallas-White <dallas-white@outlook.com>
2026-07-03 14:06:09 -0500

    push missing files

---
+++ web/app/meet/types.ts
@@ -0,0 +1,21 @@
+export interface FinalMeetResult {
+    cwaNumber: string;
+    place?: number | null;
+    grade?: string | null;
+    callName?: string | null;
+    registeredName?: string | null;
+    ownerName?: string | null;
+    ownerIDs?: string | null;
+    meetPoints?: number | null;
+    scratchDQInfo?: string | null;
+    arxEarned?: number;
+    narxEarned?: number;
+    incident?: string;
+    hcScore?: number;
+    dpcPoints?: number;
+    entryType?: string;
+    matchPoints?: number;
+    shown?: boolean
+    showPoints?: number
+    showPlacement?: number
+}
