252105c35446c8e466dcf7ea800f6b5d11b7c888
Dallas-White <dallas-white@outlook.com>
2026-07-06 18:37:43 -0500

    improve backup options

--- mysql/backup.sh
+++ mysql/backup.sh
@@ -6,21 +6,19 @@ BACKUP_FILE="$BACKUP_DIR/cwa_db_$TIMESTAMP.sql.zst"
 echo "Starting backup at $TIMESTAMP"
 
 mysqldump \
-  -h db \
+  -h 127.0.0.1 \
   -u root \
   -p${MYSQL_ROOT_PASSWORD} \
-  cwa_db | zstd -10 > "$BACKUP_FILE"
+  --all-databases | zstd -10 > "$BACKUP_FILE"
 
 if [ $? -eq 0 ]; then
   echo "Backup completed successfully: $BACKUP_FILE"
-while [ "$(du -sb "$BACKUP_DIR" | awk '{print $1}')" -gt "$BACKUP_THRESHOLD" ]; do
-    OLDEST_FILE=$(find "$BACKUP_DIR" -type f -printf '%T+ %p\n' | sort | head -n 1 | awk '{print $2}')
-    echo "Backup Size Exceded, Removing $OLDEST_FILE"
-    rm -f "$OLDEST_FILE"
-done
+  while [ "$(du -sb "$BACKUP_DIR" | awk '{print $1}')" -gt "$BACKUP_THRESHOLD" ]; do
+      OLDEST_FILE=$(find "$BACKUP_DIR" -type f -printf '%T+ %p\n' | sort | head -n 1 | awk '{print $2}')
+      echo "Backup Size Exceeded, Removing $OLDEST_FILE"
+      rm -f "$OLDEST_FILE"
+  done
 else
   echo "Backup failed"
   exit 1
 fi
-
-
